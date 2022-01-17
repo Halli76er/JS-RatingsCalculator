65f36102e40bd86412e782f741917a68d00f4df0
Sven Fleshman <sven.fleshman@gmail.com>
2022-01-17 18:46:58 +0100

    not finish yet

--- js/ratings.js
+++ js/ratings.js
@@ -1,26 +1,32 @@
-function collect_ratings(){
-    let rating = 0;
-    var ratings = {
-        count: 0, 
-        sum: 0,
-        average: 0
-      };
+/**
+ * @info RatingCalculator not finish yet ;)
+ * @info replace() wrong
+ * @info toFixed(2) wrong way
+ * 
+ */
+ function collect_ratings() {
+	let rating = 0;
+	var ratings = {
+		count: 0,
+		sum: 0,
+		average: 0
+	};
 
-const elements = document.querySelectorAll('.rating');
+	const elements = document.querySelectorAll('.rating');
 
-elements.forEach(function(element){
-    starrating= element.id.replace('star', '');
-    rating = parseInt(starrating);
-    ratings.sum += (rating * parseInt(element.value));
-    ratings.count += parseInt(element.value);
-    if (ratings.count !== 0){
-        average = ratings.sum / ratings.count;
-        ratings.average = average.toFixed(2);
-        }
-    });
-    return ratings;
+	elements.forEach(function(element) {
+		starrating = element.id.replace('star', '');
+		rating = parseInt(starrating);
+		ratings.sum += (rating * parseInt(element.value));
+		ratings.count += parseInt(element.value);
+		if (ratings.count !== 0) {
+			average = ratings.sum / ratings.count;
+			ratings.average = average.toFixed(2);
+		}
+	});
+	return ratings;
 }
 document.addEventListener('change', () => {
-    const ratings = collect_ratings();
-     document.getElementById("average").setAttribute('value', ratings.average);
+	const ratings = collect_ratings();
+	document.getElementById("average").setAttribute('value', ratings.average);
 });
