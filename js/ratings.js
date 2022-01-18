ca6277430d4ae128bcae78291095278c6cc313c0
Sven Fleshman <sven.fleshman@gmail.com>
2022-01-18 08:26:03 +0100

    RatingCalculator code finish no Comments

--- js/ratings.js
+++ js/ratings.js
@@ -14,18 +14,19 @@
 
 	const elements = document.querySelectorAll('.rating');
 
-	elements.forEach(function(element) {
-		starrating = element.id.replace('star', '');
-		rating = parseInt(starrating);
-		ratings.sum += (rating * parseInt(element.value));
+	elements.forEach(element => {
+		rating= parseInt(element.id.replace('star', ''));
+		ratings.sum += parseInt(element.value) * rating;
 		ratings.count += parseInt(element.value);
-		if (ratings.count !== 0) {
-			ratings.average = ratings.sum / ratings.count;
-		}
 	});
+
+	if (ratings.count !== 0) {
+		ratings.average = ratings.sum / ratings.count;
+	}
+
 	return ratings;
 }
 document.addEventListener('change', () => {
 	const ratings = collect_ratings();
-	document.querySelector('#average').setAttribute('value', ratings.average.toFixed(2));
+	document.querySelector('#average').value = ratings.average.toFixed(2);
 });
